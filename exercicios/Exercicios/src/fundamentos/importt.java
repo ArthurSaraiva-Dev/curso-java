62d2c1163c1bba1c6465eac53548d7328f7558a9
Arthur Saraiva <arthursaraiva.dev@gmail.com>
2025-07-24 17:39:34 -0300

    28. Import

---
+++ exercicios/Exercicios/src/fundamentos/importt.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+import java.util.Date;
+
+
+public class importt {
+    public static void main(String[] args) {
+        Date time = new Date();
+        System.out.println(time);
+    }
+}
