1afa1d7cf9731a606b24f317cad5e7ce6a853bc1
Arthur Saraiva <arthursaraiva.dev@gmail.com>
2025-07-24 10:10:22 -0300

    ajustes

--- exercicios/Exercicios/src/fundamentos/Temperatura.java
+++ exercicios/Exercicios/src/fundamentos/Temperatura.java
@@ -2,11 +2,11 @@ package fundamentos;
 
 public class Temperatura {
     public static void main(String[] args) {
-        double tempF = 98.8;
-        final int x = 32;
-        final double y = 5/9.0;
-        double celsiusResult = (tempF - x) * y;
+        double fahrenheit = 98.8;
+        final int X = 32; // Ajuste 
+        final double Y = 5/9.0; // Fator
+        double celsius = (fahrenheit - X) * Y;
 
-        System.out.println("A temperatura em Celsius é: " + celsiusResult + " graus celsius.");
+        System.out.println("A temperatura em Celsius é: " + celsius + " graus celsius.");
     }
 }
