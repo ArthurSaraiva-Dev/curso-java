99764e2c73ff129e273682f012c60ab2ed36ece0
Arthur Saraiva <arthursaraiva.dev@gmail.com>
2025-07-24 10:14:10 -0300

    refatoração

--- exercicios/Exercicios/src/fundamentos/Temperatura.java
+++ exercicios/Exercicios/src/fundamentos/Temperatura.java
@@ -2,11 +2,12 @@ package fundamentos;
 
 public class Temperatura {
     public static void main(String[] args) {
-        double fahrenheit = 98.8;
         final int X = 32; // Ajuste 
         final double Y = 5/9.0; // Fator
+        
+        double fahrenheit = 98.8;
         double celsius = (fahrenheit - X) * Y;
-
         System.out.println("A temperatura em Celsius é: " + celsius + " graus celsius.");
+        
     }
 }
