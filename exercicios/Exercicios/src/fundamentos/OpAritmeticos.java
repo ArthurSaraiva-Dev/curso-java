1f5848ff33e90934e4bbe5b9799e4d0022453465
Arthur Saraiva <arthursaraiva.dev@gmail.com>
2025-08-04 11:21:10 -0300

    43. Operadores Aritmeticos

---
+++ exercicios/Exercicios/src/fundamentos/OpAritmeticos.java
@@ -0,0 +1,29 @@
+package fundamentos;
+
+public class OpAritmeticos {
+    public static void main(String[] args) {
+        System.out.println(2 + 7);
+
+        var x = 39.67;
+        double y = 3.7;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+
+        int a = 9;
+        int b = 7;
+
+        System.out.println("-------------------");
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a / (float) b);
+
+        System.out.println(a % b);
+
+        System.out.println(x - y * a + b);
+    }
+}
