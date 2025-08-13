7108a40cef6b411a094e32256febfa8bd5326eab
ArthurSaraiva-Dev <arthursaraiva.dev@gmail.com>
2025-08-12 21:40:01 -0300

    70. WHILE

---
+++ exercicios/Exercicios/src/controle/WhileDeterminado.java
@@ -0,0 +1,12 @@
+package controle;
+
+public class WhileDeterminado {
+    public static void main(String[] args) {
+        int contador = 1;
+        while (contador <= 10) {
+            System.out.printf("Show de bola: %d\n", contador);
+            contador++;
+            // contador += 2;
+        } 
+    }
+}
