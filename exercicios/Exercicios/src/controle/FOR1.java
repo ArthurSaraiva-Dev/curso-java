e5ccb4185cfdd81c1ced2bfecc5a93eaf1fba091
ArthurSaraiva-Dev <arthursaraiva.dev@gmail.com>
2025-08-17 13:36:25 -0300

    71. FOR

---
+++ exercicios/Exercicios/src/controle/FOR1.java
@@ -0,0 +1,9 @@
+package controle;
+
+public class FOR1 {
+    public static void main(String[] args) {
+        for(int contador = 0; contador <= 10; contador++){
+            System.out.printf("contador: %d\n", contador);
+        }
+    }
+}
