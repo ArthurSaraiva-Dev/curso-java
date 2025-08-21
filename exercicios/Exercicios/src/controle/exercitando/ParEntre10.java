c6a2934684e330bb5a97aaf5d6ec243223c97bdb
ArthurSaraiva-Dev <arthursaraiva.dev@gmail.com>
2025-08-20 22:54:37 -0300

    update Desafio Par Entre 0 e 10

--- exercicios/Exercicios/src/controle/exercitando/ParEntre10.java
+++ exercicios/Exercicios/src/controle/exercitando/ParEntre10.java
@@ -6,18 +6,29 @@ public class ParEntre10 {
     public static void main(String[] args) {
         // Verificar se um núemero está entre 0 e 10 e se é par
         Scanner entrada = new Scanner(System.in);
+        String continuar = "sim";
         
-        System.out.println("Digite um número par entre 0 e 10");
-        int num = entrada.nextInt();
+        do {
+            System.out.println("Digite um número par entre 0 e 10");
+            int num = entrada.nextInt();
+         
+            if (num >= 0 && num <= 10 && num % 2 == 0) {
+                System.out.printf("O seu número [%d] é par e está entre 0 e 10.", num);
+            }else if (num % 2 == 1) {
+                System.out.printf("O número [%d] é ímpar", num);   
+            }else if (!(num >= 0 && num <= 10)) {
+                System.out.printf("O número [%d] não está no range de 0 a 10.", num);
+            }
 
-        if (num >= 0 && num <= 10 && num % 2 == 0) {
-            System.out.printf("O seu número [%d] é par e está entre 0 e 10.", num);
-        }else if (num % 2 == 1) {
-            System.out.printf("O número [%d] é ímpar", num);   
-        }else if (!(num >= 0 && num <= 10)) {
-            System.out.printf("O número [%d] não está no range de 0 a 10.", num);
-        }
+            entrada.nextLine(); // Consome a quebra de linha
 
+            System.out.println("\nDeseja Continuar? (Digite: Sim ou Não)");
+            continuar = entrada.nextLine();
+
+
+        } while (continuar.equalsIgnoreCase("sim"));
+        
+        System.out.println("Fim!");
 
         entrada.close();
     }
